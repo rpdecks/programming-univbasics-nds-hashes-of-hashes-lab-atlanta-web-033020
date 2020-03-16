4ffa7b70f4c5d5a7945c88ab850a4f97b2eb83e6
Robert Phillips <rpdecks@gmail.com>
2020-03-16 15:03:22 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -5,7 +5,7 @@
 # The tests will guide your construction.
 #
 # PORTION_1 = {
-#   label: "Kingdom",
+#   label: "Kingdom" 
 #   sub_category: {
 #     label: "Phylum",
 #     sub_category: {
@@ -42,4 +42,20 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 
+h = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+        label: "Class",
+        sub_category: {
+          label: "Order",
+            sub_category: {
+              label: "Family",
+              sub_category: {
+                    label: "Genus",
+                    sub_category: {
+                        label: "Species",
+                        sub_category: nil
+                    }}}}}}}
 end
